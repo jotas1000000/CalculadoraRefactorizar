03eea92057a0460b809e2290d646102221918279
PameeGarciaAramburo <pamela.ga.pga@gmail.com>
2021-03-27 22:46:09 -0400

    refactorizado

--- script.js
+++ script.js
@@ -1,285 +1,240 @@
 function calculadora() {
-  let t = "";
-  let v = null;
-  let b = null;
-  let o = false;
+  let texto = "";
+  let primDig = null;
+  let segDig = null;
+  let operacion = false;
   let oc = "";
   panel = document.getElementsByClassName("panel")[0];
   uno = document.getElementsByClassName("item 1")[0];
   uno.addEventListener("click", function (event) {
-    /*  console.log(event.target.innerText);
-    console.log(panel.innerText); */
     panel.innerText = event.target.innerText;
   });
   dos = document.getElementsByClassName("item 2")[0];
   dos.addEventListener("click", function (event) {
-    t = "";
-    v = null;
-    b = null;
-    o = false;
+    texto = "";
+    primDig = null;
+    segDig = null;
+    operacion = false;
     oc = "";
     panel.innerText = "";
   });
   tres = document.getElementsByClassName("item 3")[0];
   tres.addEventListener("click", function (event) {
-    t = "";
-    v = null;
-    b = null;
-    o = false;
+    texto = "";
+    primDig = null;
+    segDig = null;
+    operacion = false;
     oc = "";
     panel.innerText = "";
   });
   cuatro = document.getElementsByClassName("item 4")[0];
   cuatro.addEventListener("click", function (event) {
-    if (b !== null) {
-      if (b / 10 > 1) {
-        b = (b - (b % 10)) / 10;
-        t = `${v} ${oc} ${b}`;
-        panel.innerText = t;
-      } else if (b / 10 > 0) {
-        b = null;
-        t = `${v} ${oc}`;
-        panel.innerText = t;
+    if (segDig !== null) {
+      if (segDig / 10 > 1) {
+        segDig = (segDig - (segDig % 10)) / 10;
+        texto = `${primDig} ${oc} ${segDig}`;
+      } else if (segDig / 10 > 0) {
+        segDig = null;
+        texto = `${primDig} ${oc}`;
       }
-    } else if (o === true) {
-      o = false;
+    } else if (operacion === true) {
+      operacion = false;
       oc = "";
-      t = `${v} `;
-      panel.innerText = t;
-    } else if (v !== null) {
-      if (v / 10 > 1) {
-        v = (v - (v % 10)) / 10;
-        t = `${v} `;
-        panel.innerText = t;
-      } else if (v / 10 > 0) {
-        v = null;
-        t = ``;
-        panel.innerText = t;
+      texto = `${primDig} `;
+      panel.innerText = texto;
+    } else if (primDig !== null) {
+      if (primDig / 10 > 1) {
+        primDig = (primDig - (primDig % 10)) / 10;
+        texto = `${primDig} `;
+      } else if (primDig / 10 > 0) {
+        primDig = null;
+        texto = ``;
       }
     }
+    panel.innerText = texto;
   });
   cinco = document.getElementsByClassName("item 5")[0];
   cinco.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   seis = document.getElementsByClassName("item 6")[0];
   seis.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
+    texto = texto + `${event.target.innerText}`;
 
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   siete = document.getElementsByClassName("item 7")[0];
   siete.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
+    texto = texto + `${event.target.innerText}`;
 
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
-  ocho = document.getElementsByClassName("item 8")[0];
+  ocho = document.getElementsByClassName("item +")[0];
   ocho.addEventListener("click", function (event) {
-    if (v !== null) {
-      if (o === true) {
-        t = `${v} + `;
+    if (primDig !== null) {
+      if (operacion === true) {
+        texto = `${primDig} + `;
         oc = "+";
-        panel.innerText = t;
       } else {
-        o = true;
+        operacion = true;
         oc = "+";
-        t = `${v} + `;
-        panel.innerText = t;
+        texto = `${primDig} + `;
       }
     }
+    panel.innerText = texto;
   });
   nueve = document.getElementsByClassName("item 9")[0];
   nueve.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   diez = document.getElementsByClassName("item 10")[0];
   diez.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   once = document.getElementsByClassName("item 11")[0];
   once.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
-  doce = document.getElementsByClassName("item 12")[0];
+  doce = document.getElementsByClassName("item -")[0];
   doce.addEventListener("click", function (event) {
-    if (v !== null) {
-      if (o === true) {
-        t = `${v} - `;
+    if (primDig !== null) {
+      if (operacion === true) {
+        texto = `${primDig} - `;
         oc = "-";
-        panel.innerText = t;
       } else {
-        o = true;
+        operacion = true;
         oc = "-";
-        t = `${v} - `;
-        panel.innerText = t;
+        texto = `${primDig} - `;
       }
     }
+    panel.innerText = texto;
   });
   trece = document.getElementsByClassName("item 13")[0];
   trece.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   catorce = document.getElementsByClassName("item 14")[0];
   catorce.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
   quince = document.getElementsByClassName("item 15")[0];
   quince.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
-  sixteen = document.getElementsByClassName("item 16")[0];
+  sixteen = document.getElementsByClassName("item *")[0];
   sixteen.addEventListener("click", function (event) {
-    if (v !== null) {
-      if (o === true) {
-        t = `${v} * `;
+    if (primDig !== null) {
+      if (operacion === true) {
+        texto = `${primDig} * `;
         oc = "*";
-        panel.innerText = t;
       } else {
-        o = true;
+        operacion = true;
         oc = "*";
-        t = `${v} * `;
-        panel.innerText = t;
+        texto = `${primDig} * `;
       }
     }
+    panel.innerText = texto;
   });
   seventeen = document.getElementsByClassName("item 17")[0];
   eighteen = document.getElementsByClassName("item 18")[0];
   eighteen.addEventListener("click", function (event) {
-    t = t + `${event.target.innerText}`;
-
-    if (o === true) {
-      b = b * 10 + parseInt(event.target.innerText);
+    texto = texto + `${event.target.innerText}`;
+    if (operacion === true) {
+      segDig = segDig * 10 + parseInt(event.target.innerText);
     } else {
-      v = v * 10 + parseInt(event.target.innerText);
+      primDig = primDig * 10 + parseInt(event.target.innerText);
     }
-    console.log(v);
-    panel.innerText = t;
+    console.log(primDig);
+    panel.innerText = texto;
   });
-  nineteen = document.getElementsByClassName("item 19")[0];
+  nineteen = document.getElementsByClassName("item =")[0];
   nineteen.addEventListener("click", function (event) {
-    if (v !== null && b !== null && o !== false) {
+    if (primDig !== null && segDig !== null && o !== false) {
       if (oc === "+") {
-        v = v + b;
-        b = null;
-        o = false;
-        oc = "";
-        t = `${v}`;
-        panel.innerText = t;
-        t = "";
-        v = null;
+        primDig = primDig + segDig;
       } else if (oc === "-") {
-        v = v - b;
-        b = null;
-        o = false;
-        oc = "";
-        t = `${v}`;
-        panel.innerText = t;
-        t = "";
-        v = null;
+        primDig = primDig - segDig;
       } else if (oc === "*") {
-        v = v * b;
-        b = null;
-        o = false;
-        oc = "";
-        t = `${v}`;
-        panel.innerText = t;
-        t = "";
-        v = null;
+        primDig = primDig * segDig;
       } else {
-        v = v / b;
-        b = null;
-        o = false;
-        oc = "";
-        t = `${v}`;
-        panel.innerText = t;
-        t = "";
-        v = null;
+        primDig = primDig / segDig;
       }
     }
   });
-  twenty = document.getElementsByClassName("item 20")[0];
+  twenty = document.getElementsByClassName("item /")[0];
   twenty.addEventListener("click", function (event) {
-    if (v !== null) {
-      if (o === true) {
-        t = `${v} / `;
+    if (primDig !== null) {
+      if (operacion === true) {
+        texto = `${primDig} / `;
         oc = "/";
-        panel.innerText = t;
       } else {
-        o = true;
+        operacion = true;
         oc = "/";
-        t = `${v} / `;
-        panel.innerText = t;
+        texto = `${primDig} / `;
       }
     }
+    panel.innerText = texto;
   });
 }
 
